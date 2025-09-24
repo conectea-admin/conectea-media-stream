531b6b037a1d69251495f10a1be8775cd6da5cd1
Camilo Romero <cromero@finaipro.com>
2025-09-24 14:24:17 -0500

    chore: add base api docs

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
+import { openapi } from "@elysiajs/openapi";
 import { Elysia } from "elysia";
+import { env } from "./lib/env";
 
-const port = process.env.PORT || 3000;
+const port = env.PORT || 3000;
+const styles = await Bun.file("./src/scalar.css").text();
 
-const app = new Elysia().get("/", () => "Hello Elysia").get("/health", ({ status }) => status(200)).listen(port);
+const app = new Elysia()
+  .use(
+    openapi({
+      documentation: {
+        info: {
+          title: "Conectea Media Stream",
+          version: "1.0.0",
+          description:
+            "ConecTEA Media Stream API used to stream media from the ConecTEA platform",
+        },
+      },
+      scalar: {
+        customCss: styles,
+      },
+    }),
+  )
+  .get("/health", ({ status }) => status(200), {
+    detail: {
+      hide: true,
+    },
+  })
+  .listen(port);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
